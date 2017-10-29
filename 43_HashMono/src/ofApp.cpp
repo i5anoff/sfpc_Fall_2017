52c5a1a5eb085be34da87676a88238f3701a11ad
Niklas May <hello@niklas-may.de>
2017-10-29 14:53:22 -0400

    blanks and new lines work

--- 43_HashMono/src/ofApp.cpp
+++ 43_HashMono/src/ofApp.cpp
@@ -14,7 +14,6 @@ void ofApp::setup(){
     
     // letter position
     lines.push_back("null");
-
     
 //    letters.push_back("A");
 //    letters.push_back("B");
@@ -48,6 +47,20 @@ void ofApp::setup(){
 //--------------------------------------------------------------
 void ofApp::update(){
     
+
+    width.clear();
+    for(int i = 0; i < letters.size(); i++){
+        w = 14 * unit;
+
+        if(i % 6 == 1){
+            w = w * 1.61;
+        }
+        if(i % 4 == 1){
+            w = w * 2.61;
+        }
+        width.push_back(w);
+    }
+    
     yPos.clear();
     float yPosTemp = 0;
     for (int i = 0; i < letters.size(); i++) {
@@ -55,21 +68,20 @@ void ofApp::update(){
             yPosTemp += lineHeight;
         }
         yPos.push_back(yPosTemp);
+    }    
+    xPos.clear();
+    float xPosTemp = 0;
+    for (int i = 0; i < letters.size(); i++) {
+        if (blanks[i] == "32"){
+            xPosTemp += leading + space;
+        }if (lines[i+1] == "13"){
+            xPosTemp = 0;
+        }
+        else {
+            xPosTemp += width[i] + leading;
+        }
+        xPos.push_back(xPosTemp);
     }
-    
-    width.clear();
-    for(int i = 0; i < letters.size(); i++){
-        w = 14 * unit;
-
-//        if(i % 6 == 1){
-//            w = w * 1.61;
-//        }
-//        if(i % 4 == 1){
-//            w = w * 2.61;
-//        }
-        width.push_back(w);
-    }
-
 }
 
 //--------------------------------------------------------------
@@ -82,20 +94,9 @@ void ofApp::draw(){
     float padding = 100;
     ofTranslate(padding,padding);
     
-
-    
     for(int i = 0; i < letters.size(); i++){
-        t.draw(letters[i], 0, yPos[i], width[i], h);
+        t.draw(letters[i], xPos[i-1], yPos[i], width[i], h);
     }
-    
-//    for (int i =0; i < letters.size(); i++){
-//        t.draw(letters[i], xPos, yPos, width[i], h);
-//        xPos += width[i] + leading;
-//        if (xPos > ofGetWidth() - padding * 3){
-//            xPos = 0;
-//            yPos += h * 1.5;
-//        }
-//    }
     
 
 }
@@ -106,6 +107,7 @@ void ofApp::keyPressed(int key){
     if (key == 127 && letters.size() > 0) { //backspace
         letters.erase(letters.end()-1);
         lines.erase(lines.end()-1);
+        blanks.erase(blanks.end()-1);
     }
 }
 
@@ -113,7 +115,8 @@ void ofApp::keyPressed(int key){
 void ofApp::keyReleased(int key){
     
     if (key == 32) { // blank
-        letters.push_back("32");
+        blanks.erase(blanks.end()-1);
+        blanks.push_back("32");
     }
     if (key == 13){ // enter
         lines.erase(lines.end()-1);
@@ -123,131 +126,157 @@ void ofApp::keyReleased(int key){
     if (key == 'a') {
         letters.push_back("A");
         lines.push_back("null");
+        blanks.push_back("null");
 
     }
     if (key == 'b') {
         letters.push_back("B");
         lines.push_back("null");
+        blanks.push_back("null");
 
     }
     if (key == 'c') {
         letters.push_back("C");
         lines.push_back("null");
+        blanks.push_back("null");
 
     }
     if (key == 'd') {
         letters.push_back("D");
         lines.push_back("null");
+        blanks.push_back("null");
 
     }
     if (key == 'e') {
         letters.push_back("E");
         lines.push_back("null");
+        blanks.push_back("null");
 
     }
     if (key == 'f') {
         letters.push_back("F");
         lines.push_back("null");
+        blanks.push_back("null");
 
     }
     if (key == 'g') {
         letters.push_back("G");
         lines.push_back("null");
+        blanks.push_back("null");
 
     }
     if (key == 'h') {
         letters.push_back("H");
         lines.push_back("null");
+        blanks.push_back("null");
 
     }
     if (key == 'i') {
         letters.push_back("I");
         lines.push_back("null");
+        blanks.push_back("null");
 
     }
     if (key == 'j') {
         letters.push_back("J");
         lines.push_back("null");
+        blanks.push_back("null");
 
     }
     if (key == 'k') {
         letters.push_back("K");
         lines.push_back("null");
+        blanks.push_back("null");
 
     }
     if (key == 'l') {
         letters.push_back("L");
         lines.push_back("null");
+        blanks.push_back("null");
 
     }
     if (key == 'm') {
         letters.push_back("M");
         lines.push_back("null");
+        blanks.push_back("null");
 
     }
     if (key == 'n') {
         letters.push_back("N");
         lines.push_back("null");
+        blanks.push_back("null");
 
     }
     if (key == 'o') {
         letters.push_back("O");
         lines.push_back("null");
+        blanks.push_back("null");
 
     }
     if (key == 'p') {
         letters.push_back("P");
         lines.push_back("null");
+        blanks.push_back("null");
 
     }
     if (key == 'q') {
         letters.push_back("Q");
         lines.push_back("null");
+        blanks.push_back("null");
 
     }
     if (key == 'r') {
         letters.push_back("R");
         lines.push_back("null");
+        blanks.push_back("null");
 
     }
     if (key == 's') {
         letters.push_back("S");
         lines.push_back("null");
+        blanks.push_back("null");
 
     }
     if (key == 't') {
         letters.push_back("T");
         lines.push_back("null");
+        blanks.push_back("null");
 
     }
     if (key == 'u') {
         letters.push_back("U");
         lines.push_back("null");
+        blanks.push_back("null");
 
     }
     if (key == 'v') {
         letters.push_back("V");
         lines.push_back("null");
+        blanks.push_back("null");
 
     }
     if (key == 'w') {
         letters.push_back("W");
         lines.push_back("null");
+        blanks.push_back("null");
 
     }
     if (key == 'x') {
         letters.push_back("X");
         lines.push_back("null");
+        blanks.push_back("null");
 
     }
     if (key == 'y') {
         letters.push_back("Y");
         lines.push_back("null");
+        blanks.push_back("null");
 
     }
     if (key == 'z') {
         letters.push_back("Z");
         lines.push_back("null");
+        blanks.push_back("null");
 
     }
 
